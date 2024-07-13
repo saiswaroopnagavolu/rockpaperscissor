ec9ff3614faa8239317fd74403e80b4767ba3556
Sai Swaroop Nagavolu <154754287+saiswaroopnagavolu@users.noreply.github.com>
2024-07-13 12:16:27 +0530

    Add files via upload

---
+++ RockPaperScissor/index.js
@@ -0,0 +1,70 @@
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+let userScore = 0;
+let compScore = 0;
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
+
+const drawGame = () => {
+  msg.innerText = "Game was Draw. Play again.";
+  msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+
+  if (userScore === 5 || compScore === 5) {
+    // End game if either score reaches 5
+    msg.innerText = `Game over!! ${userScore === 5 ? "You" : "Computer"} wins!`;
+    choices.forEach(choice => choice.removeEventListener("click", playGame));
+  }
+};
+
+const playGame = (userChoice) => {
+  // Generate computer choice
+  const compChoice = genCompChoice();
+
+  if (userChoice === compChoice) {
+    // Draw Game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    if (userScore === 5 || compScore === 5) {
+      // If either score reaches 5, do not allow further clicks
+      return;
+    }
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
+
